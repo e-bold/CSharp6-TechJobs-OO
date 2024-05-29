094fd15e3da39b4272d675569728e6f4a4cf7deb
e-bold <enkhmunkh@att.net>
2024-05-28 21:57:37 -0500

    Task 6 completed -DRY. Reran all 6 tests and passed

---
+++ TechJobsOOAutoGraded6/JobField.cs
@@ -0,0 +1,33 @@
+﻿namespace TechJobsOOAutoGraded6;
+
+public abstract class JobField
+{
+    public int Id { get; }
+    private static int nextId = 1;
+    public string Value { get; set; }
+    private JobField()
+        {
+            Id = nextId;
+            nextId++;
+        }
+
+        public JobField(string value) : this()
+        {
+            Value = value;
+        }
+        public override bool Equals(object obj)
+        {
+            return obj is JobField location &&
+                   Id == location.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+        public override string ToString()
+        {
+            return Value;
+        }
+
+}
